c49603d18ad5013eb106dafd9cc4f810bf9b95c4
Nasr Soliman <nasr.soliman.11@gmail.com>
2022-12-01 15:48:04 +0200

    add new package , use @AfterReturning annotation on logging and modify results

---
+++ src/main/java/com/luv2code/aop_z_after_returning/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,17 @@
+package com.luv2code.aop_z_after_returning.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(3)
+public class MyApiAnalyticsAspect {
+
+    @Before("com.luv2code.aop_z_after_returning.aspect.LuvAopExpressions.forDaoPackageNoGetterNoSetter()")
+    public void performApiAnalytics() {
+        System.out.println("\n====>>> Performing API analytics");
+    }
+}
