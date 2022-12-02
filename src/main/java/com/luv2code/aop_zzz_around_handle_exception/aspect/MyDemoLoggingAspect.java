43552dd353d7cf0330e0eb2345ebe1ca0bbaee12
Nasr Soliman <nasr.soliman.11@gmail.com>
2022-12-02 17:15:19 +0200

    rethrowing the exception to let the client handle it

--- src/main/java/com/luv2code/aop_zzz_around_handle_exception/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aop_zzz_around_handle_exception/aspect/MyDemoLoggingAspect.java
@@ -40,7 +40,10 @@ public class MyDemoLoggingAspect {
             myLogger.warning(e.getMessage());
 
             // give user a custom message
-            result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+//            result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+
+            // rethrow exception
+            throw  e ;
         }
 
 
